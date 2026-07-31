4c62f397443afa30d3a859f2d0f4d36f43994653
Sviatoslav Tymtsiv <svtm1234567@gmail.com>
2026-07-31 19:29:11 +0300

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://magiclink-server.onrender.com';
 
 export default function Home() {
   const [email, setEmail] = React.useState('');
